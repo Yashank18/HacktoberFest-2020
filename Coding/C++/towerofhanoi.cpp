6227833642625a1c7a7967e7efb8106227b34571
dhruv <60519182+dhruvbatra810@users.noreply.github.com>
2020-10-06 22:33:20 +0530

    i realised it's not using the B or 2nd tower
    so i made it orignal one

--- Coding/C++/towerofhanoi.cpp
+++ Coding/C++/towerofhanoi.cpp
@@ -1,15 +1,15 @@
-// note reallay a tower of hanoi problem as it uses 3 towers ( i made one of mine which uses 4 towers logic mostly same as for 3towers)
+
 
 #include<bits/stdc++.h>
 using namespace std;
-void TOH(int a,int A,int B, int C,int D){
+void TOH(int a,int A,int B, int C){
     if(a > 0){
-        TOH(a-1,A,B,D,C);
-        cout<<A<<"->"<<D<<endl;
-        TOH(a-1,C,B,A,D);
+        TOH(a-1,A,C,B);
+        cout<<A<<"->"<<C<<endl;
+        TOH(a-1,B,A,C);
 
     }
 }
 int main(){
-    TOH(4,1,2,3,4);
+    TOH(4,1,2,3);
 }
